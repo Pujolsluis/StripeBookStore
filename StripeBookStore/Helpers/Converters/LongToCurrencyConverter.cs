4249a0574b5209deb572cf6b36f9fd2092e0dfdc
Luis Pujols <luispujols@hotmail.com>
2021-04-22 22:47:57 -0400

    Added a Long to Currency Converter for the price on Book items displayed

---
+++ StripeBookStore/Helpers/Converters/LongToCurrencyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace StripeBookStore.Helpers.Converters
+{
+    public class LongToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long amount = (long)value;
+
+            return $"${(decimal)amount / 100}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal amount;
+
+            decimal.TryParse((string)value, out amount);
+
+            return (long)amount * 100;
+        }
+    }
+}
