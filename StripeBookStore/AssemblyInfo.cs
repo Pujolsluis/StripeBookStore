d167894198ad07dd168ceb6993b62623390f694d
Luis Pujols <luispujols@hotmail.com>
2021-04-23 21:18:57 -0400

    Added Fonts and Styles to Theme of project

--- StripeBookStore/AssemblyInfo.cs
+++ StripeBookStore/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Roboto-Light.ttf")]
+[assembly: ExportFont("Roboto-Medium.ttf")]
+[assembly: ExportFont("Roboto-Medium.ttf")]
+[assembly: ExportFont("Roboto-Bold.ttf")]
